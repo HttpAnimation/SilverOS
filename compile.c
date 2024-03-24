f4cf9c887725b97ba7528c6d3657a02c94fc3072
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-24 08:07:03 -0700

    gtk

--- compile.c
+++ compile.c
@@ -1,25 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <gtk/gtk.h>
+#include <webkit2/webkit2.h>
 
-int main() {
-    FILE *file;
-    char *filename = "index.html";
-    char c;
+static void destroy_window_callback(GtkWidget *widget, gpointer data) {
+    gtk_main_quit();
+}
 
-    // Open the file
-    file = fopen(filename, "r");
-    if (file == NULL) {
-        fprintf(stderr, "Could not open file %s\n", filename);
-        exit(1);
-    }
+int main(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
 
-    // Read and display contents character by character
-    while ((c = fgetc(file)) != EOF) {
-        printf("%c", c);
-    }
+    // Create the main window
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+    g_signal_connect(window, "destroy", G_CALLBACK(destroy_window_callback), NULL);
 
-    // Close the file
-    fclose(file);
+    // Create the web view
+    GtkWidget *webView = webkit_web_view_new();
+
+    // Load the index.html file
+    gchar *filePath = g_strdup_printf("file://%s", "index.html");
+    webkit_web_view_load_uri(WEBKIT_WEB_VIEW(webView), filePath);
+    g_free(filePath);
+
+    // Add the web view to the main window
+    gtk_container_add(GTK_CONTAINER(window), webView);
+
+    // Show all widgets
+    gtk_widget_show_all(window);
+
+    // Start the GTK main loop
+    gtk_main();
 
     return 0;
 }
