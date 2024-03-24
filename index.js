2f2e432ab2f688db33e7ff16284e74181493eb41
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-23 21:38:35 -0700

    base

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const dockImg = document.getElementById('dockImg');
+const appOverlay = document.getElementById('appOverlay');
 
 dockImg.addEventListener('click', function() {
-    alert('Button clicked and poped') // :3
-})
+    appOverlay.style.opacity = '1'; // Show the overlay
+    setTimeout(function() {
+        appOverlay.style.opacity = '0'; // Hide the overlay after a short delay
+    }, 500); // Adjust this delay according to your preference
+});
