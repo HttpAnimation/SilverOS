eee87b7af43e205a3fb0487f0199d21dca512e1b
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-23 22:28:41 -0700

    about

--- js/goToIndexSettings.js
+++ js/goToIndexSettings.js
@@ -2,3 +2,8 @@
 function goToIndex() {
     window.location.href = 'index.html';
 }
+
+// JavaScript function to redirect to settingsAbout.html
+function goToAbout() {
+    window.location.href = 'settingsAbout.html';
+}
