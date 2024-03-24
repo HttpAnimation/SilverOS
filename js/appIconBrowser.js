d1b2bd011a23c2ee73efb75350a247107b7e838e
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-24 09:10:03 -0700

    html browser

---
+++ js/appIconBrowser.js
@@ -0,0 +1,4 @@
+// JavaScript code to handle the click event and redirect to settings.html
+document.getElementById('appIconBrowser').addEventListener('click', function() {
+    window.location.href = 'browser.html';
+});
