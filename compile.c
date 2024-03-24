75f61fd4c4772006ccb8210106d1264c2b456996
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-24 08:06:37 -0700

    displayer

--- compile.c
+++ compile.c
@@ -1,1 +1,25 @@
-// This script will compile with nodejs, npm and electron
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    FILE *file;
+    char *filename = "index.html";
+    char c;
+
+    // Open the file
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        fprintf(stderr, "Could not open file %s\n", filename);
+        exit(1);
+    }
+
+    // Read and display contents character by character
+    while ((c = fgetc(file)) != EOF) {
+        printf("%c", c);
+    }
+
+    // Close the file
+    fclose(file);
+
+    return 0;
+}
