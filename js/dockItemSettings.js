a4a1a311f3dea0fb848ad83389eb3fea39bb0e67
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-24 08:18:05 -0700

    port over

---
+++ js/dockItemSettings.js
@@ -0,0 +1,4 @@
+// JavaScript code to handle the click event and redirect to settings.html from dock item
+document.getElementById('dockItemSettings').addEventListener('click', function() {
+    window.location.href = 'settings.html';
+});
