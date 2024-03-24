a7bd5d49b1931342a1242501440bcbc46487f41b
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-24 08:17:30 -0700

    move

---
+++ js/appIconSettings.js
@@ -0,0 +1,4 @@
+// JavaScript code to handle the click event and redirect to settings.html
+document.getElementById('appIconSettings').addEventListener('click', function() {
+    window.location.href = 'settings.html';
+});
