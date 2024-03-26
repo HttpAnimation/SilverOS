681e6f78458c9c16b8816fe51d8f68d590799edd
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-25 23:48:48 -0700

    format

--- js/appIconNotes.js
+++ js/appIconNotes.js
@@ -1,4 +1,4 @@
 // JavaScript code to handle the click event and redirect to settings.html
-document.getElementById('appIconNotes').addEventListener('click', function() {
+document.getElementById('appIconNotes').addEventListener('click', function () {
     window.location.href = 'notes.html';
 });
