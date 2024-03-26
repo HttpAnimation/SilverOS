33d469c0e9edf10b99e472bb2c02459ebeec885d
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-25 23:50:55 -0700

    removes unused file

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// What does this fucking do?
-(function(details) {
-    if ( self.uBO_isolatedScriptlets === 'done' ) { return; }
-    const doc = document;
-    if ( doc.location === null ) { return; }
-    const hostname = doc.location.hostname;
-    if ( hostname !== '' && details.hostname !== hostname ) { return; }
-    const isolatedScriptlets = function() {
-// >>>> start of private namespace
