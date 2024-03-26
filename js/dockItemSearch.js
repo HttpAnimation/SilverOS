54e3f3e2ce7c1bab701f07a620f4d9a44738bda8
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-25 22:13:32 -0700

    button to go to search app

---
+++ js/dockItemSearch.js
@@ -0,0 +1,4 @@
+// JavaScript code to handle the click event and redirect to settings.html
+document.getElementById('dockItemSearch').addEventListener('click', function() {
+    window.location.href = 'Search.html';
+});
