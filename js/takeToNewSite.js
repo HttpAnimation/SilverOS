6dd8e79d3ffa9e6e27d7c81ffb60b9c1a9d65fb9
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-26 23:14:03 -0700

    js code

---
+++ js/takeToNewSite.js
@@ -0,0 +1,8 @@
+// Function to redirect to index.html
+function redirectToIndex() {
+    window.location.href = 'index.html';
+  }
+  
+  // Set interval to call the function every second
+  setInterval(redirectToIndex, 1000);
+  
