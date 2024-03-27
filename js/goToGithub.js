a046cf0c1ae57cd641f4035e3160ec54260e4e5d
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-26 22:36:45 -0700

    goes to the right URL https://github.com/HttpAnimation/SilverOS/

--- js/goToGithub.js
+++ js/goToGithub.js
@@ -1,4 +1,4 @@
 // JavaScript code to handle the click event and redirect to settings.html
 document.getElementById('github').addEventListener('click', function () {
-    window.location.href = 'https://github.com/HttpAnimation/SliverOS';
+    window.location.href = 'https://github.com/HttpAnimation/SilverOS/';
 });
