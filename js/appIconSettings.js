4fff24530f25f67d08b84e0ddf7dece91727b893
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-26 23:15:16 -0700

    heads to loading just for now until I get the loading screen working

--- js/appIconSettings.js
+++ js/appIconSettings.js
@@ -1,4 +1,4 @@
 // JavaScript code to handle the click event and redirect to settings.html
 document.getElementById('appIconSettings').addEventListener('click', function () {
-    window.location.href = 'settingsLoading.html';
+    window.location.href = 'settings.html';
 });
