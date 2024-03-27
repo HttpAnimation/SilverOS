be1a86441aa5766c8f2508bbe1cee1623e16d084
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-27 09:12:34 -0700

    points to the loading screen

--- js/appIconTerminal.js
+++ js/appIconTerminal.js
@@ -1,4 +1,4 @@
 // JavaScript code to handle the click event and redirect to settings.html
 document.getElementById('appIconTerminal').addEventListener('click', function () {
-    window.location.href = 'terminal.html';
+    window.location.href = 'loadingTerminal.html';
 });
