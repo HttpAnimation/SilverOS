cac2677eff5edd8cad727c5b6a706996a525a0a6
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-27 09:06:24 -0700

    points to loading screen

--- js/appIconPhotos.js
+++ js/appIconPhotos.js
@@ -1,4 +1,4 @@
 // JavaScript code to handle the click event and redirect to settings.html
 document.getElementById('appIconPhotos').addEventListener('click', function () {
-    window.location.href = 'photos.html';
+    window.location.href = 'loadingPhotos.html';
 });
