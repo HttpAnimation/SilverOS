6dc008dfb82788c73341f74c641c89ee6617e0cd
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-27 09:10:22 -0700

    points to the loading screen

--- js/dockItemBrowser.js
+++ js/dockItemBrowser.js
@@ -1,4 +1,4 @@
 // JavaScript code to handle the click event and redirect to settings.html from dock item
 document.getElementById('dockItemBrowser').addEventListener('click', function () {
-    window.location.href = 'browser.html';
+    window.location.href = 'loadingBrowser.html';
 });
