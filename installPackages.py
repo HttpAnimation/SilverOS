4e29500656a7d693fffe4b3d0375751d154c679c
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-27 17:37:04 -0700

    revert

--- installPackages.py
+++ installPackages.py
@@ -1,29 +1,17 @@
 import json
 import subprocess
-import sys
-import os
-
-def get_pip_command():
-    if 'win' in sys.platform:
-        print("Running on Windows system.")
-        return 'pip'
-    print("Running on Unix-like system.")
-    return 'pip3'
 
 def install_packages(packages):
-    pip_command = get_pip_command()
     for package_data in packages:
         for package_name, install_command in package_data.items():
             print(f"Installing {package_name}...")
             try:
-                subprocess.run([pip_command, 'install', package_name], check=True)
+                subprocess.run(install_command.split(), check=True)
                 print(f"{package_name} installed successfully.")
             except subprocess.CalledProcessError:
                 print(f"Failed to install {package_name}.")
 
 def main():
-    current_directory = os.getcwd()
-    print(f"Current directory: {current_directory}")
     try:
         with open('packages.json', 'r') as file:
             packages = json.load(file)
