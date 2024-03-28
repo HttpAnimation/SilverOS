d3fcdcfaab096d30812ab07680d988f23b899059
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-28 08:14:16 -0700

    loading

---
+++ js/intro.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        document.querySelector('.photo-container').style.opacity = 1;
+    }, 1000); // 1000 milliseconds = 1 second
+});
