ede1395e2fb97f843941795542813e422e1d4740
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-27 19:55:36 -0700

    compile

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 # When custom apps get added this program will compile all of them at once and enbed them in html
 gcc installPackages.c -o installPackages
 # gcc SilverOS.c -o SilverOS -lwebdriver
+gcc Display.c -o SliverOS
