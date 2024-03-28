f9f9eddde4584d5f201d469bff2544988fb82d14
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-27 19:53:51 -0700

    installer

---
+++ install.sh
@@ -0,0 +1,16 @@
+cd ~/
+git clone -b main https://github.com/HttpAnimation/SilverOS.git
+cd SilverOS
+rm geckodriver
+rm displayFlask.py
+rm flaskReq.json
+rm installPackages
+rm installPackages.c
+rm installPackages.py
+rm packages.json
+rm systeminfo.conf
+rm web_view.c
+rm compile.sh
+rm display.py
+npm install electron --save-dev
+echo "Done"
