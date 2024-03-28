cbbba33701922bb9be8b06119ef1dcdfd23472ad
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-27 19:55:23 -0700

    blanks out unneeded script

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 # When custom apps get added this program will compile all of them at once and enbed them in html
 gcc installPackages.c -o installPackages
-gcc SilverOS.c -o SilverOS -lwebdriver
+# gcc SilverOS.c -o SilverOS -lwebdriver
