ce652cc3744485213fa1ff9770248e0b4e8d8b95
HttpAnimation <sahwndouglasyt@gmail.com>
2024-04-03 14:18:11 -0700

    updates text

--- js/Shutdown.js
+++ js/Shutdown.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Get the Sleep option element
-    var sleepOption = document.querySelector("a[href='#Shutdown']");
+    var Shutdown = document.querySelector("a[href='#Shutdown']");
 
     // Add click event listener
-    sleepOption.addEventListener("click", function(event) {
+    Shutdown.addEventListener("click", function(event) {
         // Prevent the default action (following the link)
         event.preventDefault();
 
