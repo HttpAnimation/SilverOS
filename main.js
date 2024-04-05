0a912aa596ca1ef8ddf286f325902773cb6b6a1e
HttpAnimation <sahwndouglasyt@gmail.com>
2024-04-05 10:15:43 -0700

    updates the width

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let mainWindow;
 
 function createWindow() {
     mainWindow = new BrowserWindow({
-        width: 1000,
+        width: 1200,
         height: 1500,
         webPreferences: {
             nodeIntegration: true
