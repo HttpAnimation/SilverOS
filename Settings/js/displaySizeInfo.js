a1a9f9964b73f1bf1109bf43fa1eadf3bfb22055
HttpAnimation <sahwndouglasyt@gmail.com>
2024-04-06 15:46:48 -0700

    remove old js script

--- Settings/js/displaySizeInfo.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    var displaySizeInfoDiv = document.getElementById("display-size-info");
-
-    // Get display size information
-    var displaySizeInfo = "Display Size: " + window.innerWidth + "x" + window.innerHeight;
-
-    // Display display size information in the div
-    displaySizeInfoDiv.innerHTML = displaySizeInfo;
-};
