5d24fe5464d9cfe607363cc892aded47d53fca42
HttpAnimation <sahwndouglasyt@gmail.com>
2024-04-06 16:00:58 -0700

    removes nt

--- make.sh
+++ make.sh
@@ -1,3 +1,4 @@
-npm run make -- --platform win32
+# Only remove if your on nt or if your stuff is setup right unlike me :3
+#npm run make -- --platform win32
 npm run make -- --platform linux
 npm run make -- --platform darwin
