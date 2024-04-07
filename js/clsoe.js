876b5ec277d2562f715eeb484303a30719b3e34a
HttpAnimation <sahwndouglasyt@gmail.com>
2024-04-07 04:44:31 -0700

    new code

--- js/clsoe.js
+++ js/clsoe.js
@@ -1,18 +1,4 @@
-if (window.require) {
-    console.log('Renderer: Node integration is enabled.');
-    const { ipcRenderer } = require('electron');
-
-    const shutdownLink = document.getElementById('shutdown');
-    if (!shutdownLink) {
-        console.error('Shutdown link not found!');
-    } else {
-        console.log('Shutdown link found. Adding event listener.');
-        shutdownLink.addEventListener('click', (event) => {
-            event.preventDefault(); // Prevent the default anchor behavior
-            console.log('Sending shutdown-app message to main process.');
-            ipcRenderer.send('shutdown-app'); // Send an IPC message to the main process
-        });
-    }
-} else {
-    console.error('Renderer: Node integration is disabled.');
-}
+document.getElementById('shutdown').addEventListener('click', (event) => {
+    event.preventDefault();
+    window.electronAPI.shutdown();
+});
