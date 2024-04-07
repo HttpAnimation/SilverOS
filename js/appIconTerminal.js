cb78cfcf1813a52474010cce3a2f050620c0c59d
HttpAnimation <sahwndouglasyt@gmail.com>
2024-04-07 04:53:46 -0700

    - script

--- js/appIconTerminal.js
+++
@@ -1,4 +0,0 @@
-// JavaScript code to handle the click event and redirect to settings.html
-document.getElementById('appIconTerminal').addEventListener('click', function () {
-    window.location.href = 'loadingTerminal.html';
-});
