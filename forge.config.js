bfebe9043447d25daab76e6b9160bc41e4f04c60
HttpAnimation <sahwndouglasyt@gmail.com>
2024-04-10 20:08:29 -0700

    readds const

--- forge.config.js
+++ forge.config.js
@@ -1,3 +1,7 @@
+const { FusesPlugin } = require('@electron-forge/plugin-fuses');
+const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+
+
 module.exports = {
   packagerConfig: {
     asar: true,
