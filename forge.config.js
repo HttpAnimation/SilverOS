0dacda553b8f18d34b450acb1de4d2b94f5de663
HttpAnimation <sahwndouglasyt@gmail.com>
2024-04-10 20:05:28 -0700

    flatpak

--- forge.config.js
+++ forge.config.js
@@ -1,11 +1,7 @@
-const { FusesPlugin } = require('@electron-forge/plugin-fuses');
-const { FuseV1Options, FuseVersion } = require('@electron/fuses');
-
 module.exports = {
   packagerConfig: {
     asar: true,
   },
-  rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
@@ -23,14 +19,21 @@ module.exports = {
       name: '@electron-forge/maker-rpm',
       config: {},
     },
+    {
+      name: '@electron-forge/maker-flatpak',
+      config: {
+        options: {
+          arch: ['x64', 'armv7l', 'arm64'], // Specify architectures
+          runtimeVersion: '21.08' // Specify the runtime version you are targeting
+        }
+      }
+    }
   ],
   plugins: [
     {
       name: '@electron-forge/plugin-auto-unpack-natives',
       config: {},
     },
-    // Fuses are used to enable/disable various Electron functionality
-    // at package time, before code signing the application
     new FusesPlugin({
       version: FuseVersion.V1,
       [FuseV1Options.RunAsNode]: false,
