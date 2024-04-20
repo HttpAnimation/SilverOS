492b2ec84f33f498472e2e5a0ce565b074a71b7b
HttpAnimation <sahwndouglasyt@gmail.com>
2024-04-19 21:36:16 -0700

    base

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const { app, BrowserWindow } = require('electron');
+const fs = require('fs');
+const path = require('path');
 
-function createWindow() {
+function createWindowForApp(appPath) {
+  const { name, photo, file } = require(path.join(appPath, 'package.json'));
   const win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -9,10 +12,41 @@ function createWindow() {
     }
   });
 
-  win.loadFile('index.html');
+  win.loadFile(file);
+  win.setTitle(name);
+  win.setIcon(photo);
 }
 
-app.whenReady().then(createWindow);
+function createWindowsForApps() {
+  const appsDir = path.join(__dirname, 'apps');
+  fs.readdir(appsDir, (err, files) => {
+    if (err) {
+      console.error('Error reading apps directory:', err);
+      return;
+    }
+
+    files.forEach(file => {
+      const appPath = path.join(appsDir, file);
+      fs.stat(appPath, (err, stats) => {
+        if (err) {
+          console.error('Error reading file stats:', err);
+          return;
+        }
+
+        if (stats.isDirectory()) {
+          const packageJsonPath = path.join(appPath, 'package.json');
+          fs.access(packageJsonPath, fs.constants.F_OK, (err) => {
+            if (!err) {
+              createWindowForApp(appPath);
+            }
+          });
+        }
+      });
+    });
+  });
+}
+
+app.whenReady().then(createWindowsForApps);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -22,6 +56,6 @@ app.on('window-all-closed', () => {
 
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
+    createWindowsForApps();
   }
 });
